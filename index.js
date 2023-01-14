a82252cf37e491be1dee549ece2e030c88756dd4
Rutger <rutger@luinge.eu>
2023-01-14 14:35:03 +0100

    server improvements

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+
 const app = express();
 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    key: "userId",
+    secret: "this should be a long string",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 25, //1 uur
+    },
+  })
+);
+
+const saltRounds = 10;
 
 const db = mysql.createPool({
   user: "root",
@@ -13,38 +42,75 @@ const db = mysql.createPool({
   database: "logininfo",
 });
 
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user }); //req.session.user should be object containing email/username/id
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
+
 app.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
 
   db.query(
-    "INSERT INTO logindata (username, password, email) VALUES (?,?,?)",
-    [username, password, email],
+    "SELECT * FROM user_register WHERE username = ? or email = ?",
+    [username, email],
     (err, result) => {
-      console.log(err);
+      if (result.length > 0) {
+        res.send({ message: "De email of gebruikersnaam is in gebruik." });
+        console.log("does exist already");
+        return null;
+      } else {
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+          db.query(
+            "INSERT INTO user_register (username, password, email) VALUES (?,?,?)",
+            [username, hash, email],
+            (err, result) => {
+              if (err) {
+                console.log(err.message);
+              } else {
+                console.log("user in database! ", result);
+                res.send({ message: "Je registratie is voltooid!" });
+              }
+            }
+          );
+        });
+      }
     }
   );
 });
-db.qu;
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
   db.query(
-    "SELECT * FROM logindata WHERE username = ? AND password = ?",
-    [username, password],
+    "SELECT * FROM user_register WHERE username = ?",
+    username,
     (err, result) => {
-     
       if (err) {
         res.send({ err: err });
       }
-      console.log(result);
+
       if (result.length > 0) {
-        res.send(result);
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if (response) {
+            req.session.user = {
+              username: result[0].username,
+              id: result[0].id,
+              email: result[0].email,
+            };
+
+            res.send(req.session.user);
+          } else {
+            res.send({ message: "wrong password for this user!" });
+          }
+        });
       } else {
-        res.send({ message: "wrong username/password combination" });
+        res.send({ message: "user doesnt exist" });
       }
     }
   );
